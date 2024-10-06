daac9965b8b433db36f9cef4070f7721e975ed1e
Aryeahhh <aryapatel0107@gmail.com>
2024-10-06 05:15:27 -0400

    Models

---
+++ FastPoll/Models/Option.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastPoll.Models
+{
+    public class Option
+    {
+        public int OptionId { get; set; }
+
+        [Required]
+        public int PollId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Text { get; set; }
+
+        [Required]
+        public int Votes { get; set; }
+
+        public Poll Poll { get; set; }
+    }
+}
---
+++ FastPoll/Models/Poll.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis.Options;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastPoll.Models
+{
+    public class Poll
+    {
+        public int PollId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Question { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CreatedBy { get; set; }
+
+        public List<Option> Options { get; set; } = new List<Option>();
+    }
+}
